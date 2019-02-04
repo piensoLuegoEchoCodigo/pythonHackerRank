03451a2759f524966683c56469fbd7652fe0e752
piensoLuegoEchoCodigo <47308567+piensoLuegoEchoCodigo@users.noreply.github.com>
2019-02-03 19:56:43 -0500

    Add files via upload

---
+++ weird.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+def isEven(n):
+	if n % 2 == 0:
+		return True
+	return 
+
+if n > 100 or n < 1:
+	print ("Error")
+
+if isEven(n) and n>=2 and n<=5:
+	print ("Not Weird")
+elif isEven(n) and n>=6 and n<=20:
+	print ("Weird")
+elif isEven(n) and n>20:
+	print ("Not Weird")
+else:
+	 print ("Weird")
